762a2b8ce8c74e4533ae5300c59a776b967c38fd
songhaechan <bukak2019@naver.com>
2024-08-10 22:49:40 +0900

    setting: docker-compose, yml setting

--- src/main/java/com/amorgakco/backend/notification/infrastructure/consumer/sms/CoolSmsConsumer.java
+++ src/main/java/com/amorgakco/backend/notification/infrastructure/consumer/sms/CoolSmsConsumer.java
@@ -2,9 +2,9 @@ package com.amorgakco.backend.notification.infrastructure.consumer.sms;
 
 import com.amorgakco.backend.member.domain.Member;
 import com.amorgakco.backend.notification.infrastructure.consumer.NotificationRequest;
+import com.amorgakco.backend.notification.infrastructure.consumer.slack.SlackSender;
 
 import net.nurigo.sdk.message.model.Message;
-import net.nurigo.sdk.message.request.SingleMessageSendingRequest;
 import net.nurigo.sdk.message.service.DefaultMessageService;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -16,19 +16,23 @@ public class CoolSmsConsumer implements SmsSender {
 
     private final DefaultMessageService messageService;
     private final String serverPhoneNumber;
+    private final SlackSender slackSender;
 
     public CoolSmsConsumer(
             final DefaultMessageService messageService,
-            @Value("${server-phone-number}") final String serverPhoneNumber) {
+            @Value("${server-phone-number}") final String serverPhoneNumber,
+            final SlackSender slackSender) {
         this.messageService = messageService;
         this.serverPhoneNumber = serverPhoneNumber;
+        this.slackSender = slackSender;
     }
 
     @Override
     @RabbitListener(queues = "sms")
     public void send(final NotificationRequest request) {
-        final Message message = createMessage(request);
-        messageService.sendOne(new SingleMessageSendingRequest(message));
+        slackSender.sendNotification(request);
+        //        final Message message = createMessage(request);
+        //        messageService.sendOne(new SingleMessageSendingRequest(message));
     }
 
     private Message createMessage(final NotificationRequest request) {
