f9853b0206ddc06ca162af43b2e81bd50951daa2
songhaechan <bukak2019@naver.com>
2024-09-22 22:02:11 +0900

    test: redis testcontainer

--- src/test/java/com/amorgakco/backend/container/TestContainerConfig.java
+++ src/test/java/com/amorgakco/backend/container/TestContainerConfig.java
@@ -1,40 +1,43 @@
 package com.amorgakco.backend.container;
 
 import com.redis.testcontainers.RedisContainer;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.testcontainers.containers.MySQLContainer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.testcontainers.utility.DockerImageName;
 
-
+@Configuration
+@Profile("test")
 public class TestContainerConfig
         implements ApplicationContextInitializer<ConfigurableApplicationContext> {
 
+    private static final String REDISSON_HOST_PREFIX = "redis://";
     private static final RedisContainer REDIS_CONTAINER =
-            new RedisContainer(DockerImageName.parse("redis:latest")).withExposedPorts(6379);
-    private static final MySQLContainer MYSQL_CONTAINER =
-            new MySQLContainer(DockerImageName.parse("mysql:8.0.22"))
-                    .withDatabaseName("amorgakco")
-                    .withUsername("admin")
-                    .withPassword("1234");
+            new RedisContainer(DockerImageName.parse("redis:latest"))
+                    .withExposedPorts(6379);
+
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer().setAddress(REDISSON_HOST_PREFIX + REDIS_CONTAINER.getHost() + ":" + REDIS_CONTAINER.getFirstMappedPort());
+        return Redisson.create(config);
+    }
 
     static {
         REDIS_CONTAINER.start();
-        MYSQL_CONTAINER.start();
     }
 
     @Override
     public void initialize(final ConfigurableApplicationContext applicationContext) {
-        final String jdbcUrl = MYSQL_CONTAINER.getJdbcUrl();
-        final String username = MYSQL_CONTAINER.getUsername();
-        final String password = MYSQL_CONTAINER.getPassword();
         TestPropertyValues.of(
                         "spring.data.redis.host=" + REDIS_CONTAINER.getHost(),
-                        "spring.data.redis.port=" + REDIS_CONTAINER.getFirstMappedPort(),
-                        "spring.datasource.url=" + jdbcUrl,
-                        "spring.datasource.username=" + username,
-                        "spring.datasource.password=" + password)
+                        "spring.data.redis.port=" + REDIS_CONTAINER.getFirstMappedPort())
                 .applyTo(applicationContext.getEnvironment());
     }
 }
