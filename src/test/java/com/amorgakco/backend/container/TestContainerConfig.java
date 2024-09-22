790e547ad80a25c113726c6e816c24720f837f58
songhaechan <bukak2019@naver.com>
2024-09-22 21:11:58 +0900

    test: redis testcontainer

--- src/test/java/com/amorgakco/backend/container/TestContainerConfig.java
+++ src/test/java/com/amorgakco/backend/container/TestContainerConfig.java
@@ -4,6 +4,7 @@ import com.redis.testcontainers.RedisContainer;
 import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 
@@ -12,16 +13,28 @@ public class TestContainerConfig
 
     private static final RedisContainer REDIS_CONTAINER =
             new RedisContainer(DockerImageName.parse("redis:latest")).withExposedPorts(6379);
+    private static final MySQLContainer MYSQL_CONTAINER =
+            new MySQLContainer(DockerImageName.parse("mysql:8.0.22"))
+                    .withDatabaseName("amorgakco")
+                    .withUsername("admin")
+                    .withPassword("1234");
 
     static {
         REDIS_CONTAINER.start();
+        MYSQL_CONTAINER.start();
     }
 
     @Override
     public void initialize(final ConfigurableApplicationContext applicationContext) {
+        final String jdbcUrl = MYSQL_CONTAINER.getJdbcUrl();
+        final String username = MYSQL_CONTAINER.getUsername();
+        final String password = MYSQL_CONTAINER.getPassword();
         TestPropertyValues.of(
                         "spring.data.redis.host=" + REDIS_CONTAINER.getHost(),
-                        "spring.data.redis.port=" + REDIS_CONTAINER.getFirstMappedPort())
+                        "spring.data.redis.port=" + REDIS_CONTAINER.getFirstMappedPort(),
+                        "spring.datasource.url=" + jdbcUrl,
+                        "spring.datasource.username=" + username,
+                        "spring.datasource.password=" + password)
                 .applyTo(applicationContext.getEnvironment());
     }
 }
