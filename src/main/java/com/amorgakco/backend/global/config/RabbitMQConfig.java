ac6de8351c4dbf864b9eb6e64fb1477ff6ba9d92
songhaechan <bukak2019@naver.com>
2024-09-22 16:39:00 +0900

    fix: rabbitmq test 제외

--- src/main/java/com/amorgakco/backend/global/config/RabbitMQConfig.java
+++ src/main/java/com/amorgakco/backend/global/config/RabbitMQConfig.java
@@ -18,11 +18,12 @@ import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @EnableRabbit
 @Configuration
 @RequiredArgsConstructor
-//@Profile("!test")
+@Profile("!test")
 public class RabbitMQConfig {
 
     private final RabbitMQProperties properties;
