105d94ee92ea59a9d912049019d5865323240dbb
songhaechan <bukak2019@naver.com>
2024-09-22 21:35:44 +0900

    test: redis testcontainer

--- src/main/java/com/amorgakco/backend/global/config/redisson/RedissonConfig.java
+++ src/main/java/com/amorgakco/backend/global/config/redisson/RedissonConfig.java
@@ -5,7 +5,9 @@ import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class RedissonConfig {
     private static final String REDISSON_HOST_PREFIX = "redis://";
     @Value("${spring.redis.host}")
