c5e6d255c0a15763bbb4e991f1b8cd6c5b3fc247
songhaechan <bukak2019@naver.com>
2024-11-09 16:13:03 +0900

    docs: API docs 수정

---
+++ main/src/main/java/com/amorgakco/backend/group/repository/GroupRepository.java
@@ -0,0 +1,23 @@
+package com.amorgakco.backend.group.repository;
+
+import com.amorgakco.backend.group.domain.Group;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GroupRepository extends JpaRepository<Group, Long> {
+
+    @Query("select g from Group g join fetch g.host where g.id = :groupId")
+    Optional<Group> findByIdWithHost(Long groupId);
+
+    @Query("select g from Group g where g.location.cellToken in :cellTokens ")
+    List<Group> findByCellToken(List<String> cellTokens);
+
+    @Query("select g from Group g join fetch g.groupParticipants gp join fetch gp.member"
+        + " where g.id = "
+        + ":groupId")
+    Optional<Group> findByIdWithParticipant(Long groupId);
+}
