a4ff28dfe69aa42c311393e0e4881e7e4723aa10
songhaechan <bukak2019@naver.com>
2024-12-13 23:10:38 +0900

    fix: cors 배포 도메인 추가

--- main/src/main/java/com/amorgakco/backend/global/config/CorsConfig.java
+++ main/src/main/java/com/amorgakco/backend/global/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000")
+            .allowedOrigins("http://localhost:3000","https://amorgakco.store")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
             .allowCredentials(true)
             .exposedHeaders(HttpHeaders.LOCATION);
