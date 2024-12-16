211d0ee809c8e67bb78358ea0cec35b850590b6f
songhaechan <bukak2019@naver.com>
2024-12-16 22:54:41 +0900

    fix: Rabbit Config 수정 message converter

--- main/src/main/java/com/amorgakco/backend/global/config/RabbitMQConfig.java
+++ main/src/main/java/com/amorgakco/backend/global/config/RabbitMQConfig.java
@@ -89,6 +89,13 @@ public class RabbitMQConfig {
     }
 
     @Bean
+    public RabbitTemplate rabbitTemplate() {
+        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory());
+        rabbitTemplate.setMessageConverter(messageConverter());
+        return rabbitTemplate;
+    }
+
+    @Bean
     public MessageConverter messageConverter() {
         return new Jackson2JsonMessageConverter();
     }
--- notification/src/main/java/com/amorgakco/notification/config/RabbitMQConfig.java
+++ notification/src/main/java/com/amorgakco/notification/config/RabbitMQConfig.java
@@ -6,6 +6,7 @@ import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
@@ -38,6 +39,13 @@ public class RabbitMQConfig {
         connectionFactory.setUsername(properties.username());
         connectionFactory.setPassword(properties.password());
         return connectionFactory;
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate() {
+        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory());
+        rabbitTemplate.setMessageConverter(messageConverter());
+        return rabbitTemplate;
     }
 
     @Bean
