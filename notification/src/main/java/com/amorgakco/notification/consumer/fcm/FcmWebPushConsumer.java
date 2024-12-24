bea4f53a044156a632f452498487e3d19716a22e
songhaechan <bukak2019@naver.com>
2024-12-24 23:39:14 +0900

    chore: FCM 메세지 로깅

--- notification/src/main/java/com/amorgakco/notification/consumer/fcm/FcmWebPushConsumer.java
+++ notification/src/main/java/com/amorgakco/notification/consumer/fcm/FcmWebPushConsumer.java
@@ -1,13 +1,17 @@
 package com.amorgakco.notification.consumer.fcm;
 
+import com.amorgakco.notification.consumer.slack.SlackSender;
 import com.amorgakco.notification.dto.FcmMessageRequest;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.Message;
 import com.google.firebase.messaging.WebpushConfig;
 import com.google.firebase.messaging.WebpushNotification;
 
+import java.util.concurrent.ExecutionException;
 import lombok.RequiredArgsConstructor;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
@@ -15,15 +19,19 @@ import java.io.IOException;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class FcmWebPushConsumer {
 
     private final ObjectMapper objectMapper;
 
+
     @RabbitListener(queues = "fcm")
-    public void send(final String message) throws IOException {
-        FcmMessageRequest request = objectMapper.readValue(message,
+    public void send(final String notification) throws IOException, ExecutionException, InterruptedException {
+        final FcmMessageRequest request = objectMapper.readValue(notification,
             FcmMessageRequest.class);
-        createMessage(request.token(),request.title(),request.content());
+        final Message message = createMessage(request.token(), request.title(), request.content());
+        final String response = FirebaseMessaging.getInstance().sendAsync(message).get();
+        log.info(response);
     }
 
     public Message createMessage(final String token, final String title, final String content) {
