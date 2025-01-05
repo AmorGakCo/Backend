990f94a64f914a33ea96e3e29677b14da9f98ca7
songhaechan <bukak2019@naver.com>
2025-01-05 21:03:50 +0900

    setting: prefetch 5

--- notification/src/main/java/com/amorgakco/notification/config/RabbitMQConfig.java
+++ notification/src/main/java/com/amorgakco/notification/config/RabbitMQConfig.java
@@ -26,8 +26,7 @@ public class RabbitMQConfig {
         final SimpleRabbitListenerContainerFactory factory =
                 new SimpleRabbitListenerContainerFactory();
         factory.setConnectionFactory(connectionFactory);
-        factory.setPrefetchCount(20);
-        factory.setConcurrentConsumers(5);
+        factory.setPrefetchCount(5);
         return factory;
     }
 
