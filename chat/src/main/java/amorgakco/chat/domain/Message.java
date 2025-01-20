00520c9bef84916d58e583ced714effa1fddb9c2
songhaechan <bukak2019@naver.com>
2025-01-20 11:15:34 +0900

    setting: build.gradle Prometheus, actuator

--- chat/src/main/java/amorgakco/chat/domain/Message.java
+++ chat/src/main/java/amorgakco/chat/domain/Message.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "text")
+@Entity
 @Getter
 public class Message {
     @Id
